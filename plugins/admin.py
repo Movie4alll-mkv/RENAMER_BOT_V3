22135982d46a1b3fd549629da66525a215e0da4c
MeharFaizan1 <101054964+MeharFaizan1@users.noreply.github.com>
2022-11-21 21:02:41 +0500

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -2,7 +2,7 @@ import os
 from pyrogram import Client, filters
 from helper.date import add_date
 from helper.database import uploadlimit , usertype,addpre
-ADMIN = int(os.environ.get("ADMIN", "1430593323'))
+ADMIN = int(os.environ.get("ADMIN", '1430593323'))
 from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 
 
