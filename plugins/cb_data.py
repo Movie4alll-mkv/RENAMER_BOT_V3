09f410ce2e16bc712f791608fde5694d85150a65
MeharFaizan1 <101054964+MeharFaizan1@users.noreply.github.com>
2022-11-21 19:58:04 +0500

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -14,7 +14,7 @@ from helper.ffmpeg import take_screen_shot,fix_thumb
 from helper.progress import humanbytes
 from helper.set import escape_invalid_curly_brackets
 
-log_channel = int(os.environ.get("LOG_CHANNEL", "AnimeColony"))
+log_channel = int(os.environ.get("LOG_CHANNEL", "-1001858175451"))
 
 API_ID = int(os.environ.get("API_ID", "6534707"))
 
